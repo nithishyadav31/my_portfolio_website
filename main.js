edd0b714fd8847e12c7df068733f8456a79f59ee
chinnalanithishyadav <23p65a6706@vbithyd.ac.in>
2026-02-16 22:49:36 +0530

    Remove scroll animations and fix profile image blur issues

--- main.js
+++ main.js
@@ -128,18 +128,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 1500,
-    delay: 200,
-    // reset: true // Animations repeat
-})
-
-sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'right' })
-sr.reveal(`.home__name, .home__info,
-            .about__container .section__title-1, .about__info,
-            .contact__social, .contact__data`, { origin: 'left' })
-sr.reveal(`.services__card, .projects__card`, { interval: 100 })
-sr.reveal(`.education, .skills`, { origin: 'bottom' })
